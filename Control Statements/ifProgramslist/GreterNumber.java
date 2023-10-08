dbeabe207a041efde464cc4a772df7c0dd57ef01
jaipu <jaipu@LAPTOP-3TCEP752>
2023-10-08 18:20:08 +0530

    Greter than value

---
+++ Control Statements/ifProgramslist/GreterNumber.java
@@ -0,0 +1,22 @@
+package ifProgramslist;
+
+public class GreterNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=100;
+		int b=20;
+		
+		if(a>b) {
+		System.out.println("a is greater than b  and a value is :  "+a);
+		}
+		
+		else {
+			
+			
+			System.out.println("b is greater than a and b value is : "+b);
+		}
+
+	}
+
+}

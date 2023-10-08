5ddd96e9094dddc376c3019f7e7d7e57529c9e06
jaipu <jaipu@LAPTOP-3TCEP752>
2023-10-08 17:55:43 +0530

    Table print

---
+++ Control Statements/forLoopProgramsList/PrintTable.java
@@ -0,0 +1,16 @@
+package forLoopProgramsList;
+
+public class PrintTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int table=2;
+		for(int i=1;i<=10;i++) {
+			
+			System.out.println(table+" * "+i+" = "+(table*i));
+		}
+
+	}
+
+}

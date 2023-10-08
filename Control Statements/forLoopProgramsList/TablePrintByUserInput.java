89285722e8de69a3f91850ffe0426de381adf006
jaipu <jaipu@LAPTOP-3TCEP752>
2023-10-08 18:00:53 +0530

    print table by user input

---
+++ Control Statements/forLoopProgramsList/TablePrintByUserInput.java
@@ -0,0 +1,20 @@
+
+package forLoopProgramsList;
+import java.util.*;
+public class TablePrintByUserInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("Please enter your number which you want print a table");
+		int table=sc.nextInt();
+		
+		for(int i=1;i<=10;i++) {
+			
+			System.out.println(table+" * "+i+" = "+(table*i));
+		}
+
+	}
+
+}
